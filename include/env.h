a06c036a3a5162bd93957de8baf1b6e413aa711c
Paweł Czarny <141430393+ImPablo07@users.noreply.github.com>
2026-06-06 13:40:50 +0200

    Plik env.h
    Utworzenie pliku nagłówkowego env.h do zarządzania zmiennymi środowiskowymi

---
+++ include/env.h
@@ -0,0 +1,15 @@
+#ifndef ENV_H
+# define ENV_H
+
+typedef struct s_env
+{
+    char            *key;
+    char            *value;
+    struct s_env    *next;
+} t_env;
+
+t_env  *env_init(char **envp);
+char   *env_get(t_env *env, const char *key);
+void    env_destroy(t_env *env);
+
+#endif
