36437ec20481de459a69be7c745bc02bbeb19d76
Paweł Czarny <141430393+ImPablo07@users.noreply.github.com>
2026-06-06 15:14:33 +0200

    Plik minishell.h
    Dodanie definicji struktury shella w pliku minishell.h

--- include/minishell.h
+++ include/minishell.h
@@ -1,1 +1,20 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
 
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+
+# include "env.h"
+# include "signals.h"
+# include "pipes.h"
+# include "redirections.h"
+# include "utils.h"
+
+typedef struct s_shell
+{
+    t_env   *env;
+    int     last_exit_status;
+} t_shell;
+
+#endif
