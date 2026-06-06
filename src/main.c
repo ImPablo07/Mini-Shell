3cee481de80ba1714ec444d084a91327bce01954
Paweł Czarny <141430393+ImPablo07@users.noreply.github.com>
2026-06-06 15:17:20 +0200

    Minimalny start projektu
    Postawienie środowiska shella i obsługa sygnałów

--- src/main.c
+++ src/main.c
@@ -1,1 +1,17 @@
+#include "minishell.h"
 
+int main(int argc, char **argv, char **envp)
+{
+    t_shell shell;
+
+    (void)argc;
+    (void)argv;
+
+    shell.env = env_init(envp);
+    shell.last_exit_status = 0;
+
+    init_signals();
+
+    env_destroy(shell.env);
+    return (0);
+}
