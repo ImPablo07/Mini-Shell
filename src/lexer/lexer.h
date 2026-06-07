9d8e85960a16fbe7eb6933cfb2ecb4b6acc025c3
Paweł Czarny <141430393+ImPablo07@users.noreply.github.com>
2026-06-07 15:25:21 +0200

    Plik lexer.h
    Dodanie pliku nagłówkowego lexer.h z nadaną strukturą i typem tokenu

---
+++ src/lexer/lexer.h
@@ -0,0 +1,14 @@
+typedef enum e_token_type
+{
+    TOKEN_WORD,
+    TOKEN_PIPE,
+    TOKEN_REDIR_IN,
+    TOKEN_REDIR_OUT
+} t_token_type;
+
+typedef struct s_token
+{
+    t_token_type type;
+    char *value;
+    struct s_token *next;
+} t_token;
