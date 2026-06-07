5c130ec3797b083e36ea78623385f87ffbcd4d33
Paweł Czarny <141430393+ImPablo07@users.noreply.github.com>
2026-06-07 13:50:40 +0200

    Plik shell_loop.h - deklaracja funkcji pętli shella
    Stworzenie pliku nagłówkowego dla funkcjonalności pętli shella

---
+++ src/shell/shell_loop.h
@@ -0,0 +1,13 @@
+#ifndef SHELL_LOOP_H
+# define SHELL_LOOP_H
+
+# include "minishell.h"
+
+/**
+ * @brief Uruchamia główną pętlę shella.
+ *
+ * @param shell Struktura przechowująca stan shella.
+ */
+void shell_loop(t_shell *shell);
+
+#endif
