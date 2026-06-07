9d54e7548e9abdf90044869fd7addda59a31390c
Paweł Czarny <141430393+ImPablo07@users.noreply.github.com>
2026-06-07 13:47:30 +0200

    Plik main.c wraz z komentarzami
    Inicjalizacja shella i start pętli REPL

--- src/main.c
+++ src/main.c
@@ -1,5 +1,31 @@
-#include "minishell.h"
+/******************************************************************************
+ * @file main.c
+ * @author Paweł Czarny
+ * @brief Punkt wejścia programu MiniShell.
+ *
+ * Inicjalizuje:
+ * - środowisko procesu,
+ * - obsługę sygnałów,
+ * - główną strukturę shella.
+ *
+ * Następnie uruchamia pętlę REPL.
+ ******************************************************************************/
 
+#include "minishell.h"
+#include "shell_loop.h"
+
+/**
+ * @brief Główna funkcja programu.
+ *
+ * Tworzy i inicjalizuje strukturę shella,
+ * uruchamia obsługę sygnałów oraz główną pętlę REPL.
+ *
+ * @param argc Liczba argumentów programu.
+ * @param argv Argumenty programu.
+ * @param envp Zmienne środowiskowe procesu.
+ *
+ * @return Kod zakończenia procesu.
+ */
 int main(int argc, char **argv, char **envp)
 {
     t_shell shell;
@@ -12,6 +38,9 @@ int main(int argc, char **argv, char **envp)
 
     init_signals();
 
+    shell_loop();
+
     env_destroy(shell.env);
+
     return (0);
 }
