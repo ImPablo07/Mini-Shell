e999f5deb3a11404a6e0fbd33a57aed7a110348b
Paweł Czarny <141430393+ImPablo07@users.noreply.github.com>
2026-06-07 16:04:36 +0200

    Dodana jedna deklaracja funkcji
    Dodanie deklaracji funkcji ft_substr do pliku nagłówkowego utils.h

--- include/utils.h
+++ include/utils.h
@@ -1,7 +1,10 @@
 #ifndef UTILS_H
 # define UTILS_H
 
+# include <stddef.h>
+
+size_t  ft_strlen(const char *s);
 char    *ft_strdup(const char *s);
-size_t   ft_strlen(const char *s);
+char    *ft_substr(const char *s, unsigned int start, size_t len);
 
 #endif
