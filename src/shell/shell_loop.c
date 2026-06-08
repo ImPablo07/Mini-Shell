78951508052dc5ecdb425bbec880a4e6c7dff148
Paweł Czarny <141430393+ImPablo07@users.noreply.github.com>
2026-06-08 10:53:47 +0200

    Zmiany w kodzie funkcji shell_loop
    Zmiany w fukncji shell_loop w celu poprawy czytelności i obsługi

--- src/shell/shell_loop.c
+++ src/shell/shell_loop.c
@@ -1,12 +1,35 @@
+/******************************************************************************
+ * @file shell_loop.c
+ * @author Paweł Czarny
+ * @brief Główna pętla REPL MiniShell.
+ ******************************************************************************/
+
 #include <stdlib.h>
+
 #include <readline/readline.h>
 #include <readline/history.h>
 
 #include "shell_loop.h"
+#include "lexer.h"
+#include "parser.h"
 
+/**
+ * @brief Główna pętla REPL.
+ *
+ * Odpowiada za:
+ * - odczyt wejścia użytkownika,
+ * - zapis historii,
+ * - uruchomienie lexer'a,
+ * - uruchomienie parsera,
+ * - wyświetlenie wyników debugowania.
+ *
+ * @param shell Struktura przechowująca stan shella.
+ */
 void shell_loop(t_shell *shell)
 {
-    char *line;
+    char        *line;
+    t_token     *tokens;
+    t_command   *commands;
 
     (void)shell;
 
@@ -19,6 +42,23 @@ void shell_loop(t_shell *shell)
 
         if (*line)
             add_history(line);
+
+        tokens = tokenize(line);
+
+        if (tokens)
+        {
+            print_tokens(tokens);
+
+            commands = parse_tokens(tokens);
+
+            if (commands)
+            {
+                print_commands(commands);
+                free_commands(commands);
+            }
+
+            free_tokens(tokens);
+        }
 
         free(line);
     }
