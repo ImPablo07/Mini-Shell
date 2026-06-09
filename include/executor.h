be53aa1330e1d51575f376953816e31b38bf0522
Paweł Czarny <141430393+ImPablo07@users.noreply.github.com>
2026-06-09 17:49:25 +0200

    Aktualizacja executor.h
    Dodanie deklaracji funkcji execute_command

--- include/executor.h
+++ include/executor.h
@@ -1,1 +1,14 @@
+#ifndef EXECUTOR_H
+# define EXECUTOR_H
 
+# include "parser.h"
+
+/**
+ * @brief Wykonuje pojedyncze polecenie.
+ *
+ * @param cmd Polecenie do wykonania.
+ * @return Kod zakończenia procesu.
+ */
+int execute_command(t_command *cmd);
+
+#endif
