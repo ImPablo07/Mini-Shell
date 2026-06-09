c56124394be1e52ad86a3575dbc23bd0d4998c25
Paweł Czarny <141430393+ImPablo07@users.noreply.github.com>
2026-06-09 17:51:35 +0200

    Wykonanie komendy
    Implementacja funkcji execute_command do wywołania komendy w dziedzicznym procesie

--- src/executor/executor.c
+++ src/executor/executor.c
@@ -1,1 +1,41 @@
+/******************************************************************************
+ * @file executor.c
+ * @brief Wykonywanie pojedynczych poleceń.
+ ******************************************************************************/
 
+#include <unistd.h>
+#include <sys/wait.h>
+
+#include "executor.h"
+
+/**
+ * @brief Wykonuje pojedyncze polecenie.
+ *
+ * @param cmd Polecenie.
+ * @return Kod zakończenia procesu.
+ */
+int execute_command(t_command *cmd)
+{
+    pid_t pid;
+    int status;
+
+    pid = fork();
+
+    if (pid == -1)
+        return (1);
+
+    if (pid == 0)
+    {
+        execvp(cmd->argv[0], cmd->argv);
+
+        perror("execvp");
+        _exit(EXIT_FAILURE);
+    }
+
+    waitpid(pid, &status, 0);
+
+    if (WIFEXITED(status))
+        return (WEXITSTATUS(status));
+
+    return (1);
+}
