57a520c6681f9055b7172da87f908e3988010474
Paweł Czarny <141430393+ImPablo07@users.noreply.github.com>
2026-06-10 18:14:29 +0200

    Document builtin_pwd function
    Add function documentation for builtin_pwd.

--- src/builtins/builtin_pwd.c
+++ src/builtins/builtin_pwd.c
@@ -1,8 +1,23 @@
+/******************************************************************************
+ * @file builtin_pwd.c
+ * @author Paweł Czarny
+ * @brief Implementacja polecenia pwd.
+ ******************************************************************************/
+
 #include <stdio.h>
 #include <unistd.h>
 
 #include "builtins.h"
 
+/**
+ * @brief Wyświetla aktualny katalog roboczy.
+ *
+ * Funkcja korzysta z getcwd().
+ *
+ * @return
+ * - 0 przy sukcesie,
+ * - 1 przy błędzie.
+ */
 int builtin_pwd(void)
 {
     char cwd[4096];
