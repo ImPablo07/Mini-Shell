f04320ff5c3307ba39266218139c603467dfa5fc
Paweł Czarny <141430393+ImPablo07@users.noreply.github.com>
2026-06-10 18:12:41 +0200

    Implement documentation for builtin_echo command
    Added documentation for builtin_echo function.

--- src/builtins/builtin_echo.c
+++ src/builtins/builtin_echo.c
@@ -1,7 +1,22 @@
+/******************************************************************************
+ * @file builtin_echo.c
+ * @author Paweł Czarny
+ * @brief Implementacja polecenia echo.
+ ******************************************************************************/
+
 #include <stdio.h>
 
 #include "builtins.h"
 
+/**
+ * @brief Wypisuje argumenty na standardowe wyjście.
+ *
+ * Implementacja podstawowej wersji polecenia echo.
+ *
+ * @param cmd Polecenie wraz z argumentami.
+ *
+ * @return Zawsze zwraca 0.
+ */
 int builtin_echo(t_command *cmd)
 {
     int i;
