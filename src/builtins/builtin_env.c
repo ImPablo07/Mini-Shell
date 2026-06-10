bc969d134232b94cabc17c0317b3236af4447f1a
Paweł Czarny <141430393+ImPablo07@users.noreply.github.com>
2026-06-10 18:18:02 +0200

    Document builtin_env function in builtin_env.c
    Added documentation for the builtin_env function.

--- src/builtins/builtin_env.c
+++ src/builtins/builtin_env.c
@@ -1,7 +1,23 @@
+/******************************************************************************
+ * @file builtin_env.c
+ * @author Paweł Czarny
+ * @brief Implementacja polecenia env.
+ ******************************************************************************/
+
 #include <stdio.h>
 
 #include "builtins.h"
 
+/**
+ * @brief Wyświetla wszystkie zmienne środowiskowe.
+ *
+ * Funkcja iteruje po liście środowiska
+ * przechowywanej przez shell.
+ *
+ * @param shell Struktura shella.
+ *
+ * @return Zawsze zwraca 0.
+ */
 int builtin_env(t_shell *shell)
 {
     t_env *env;
