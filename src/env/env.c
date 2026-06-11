07c633fdac3b610f4d397a530d419b0b5c9481ce
Paweł Czarny <141430393+ImPablo07@users.noreply.github.com>
2026-06-11 20:42:26 +0200

    Implement env_find function to locate environment variable

--- src/env/env.c
+++ src/env/env.c
@@ -17,3 +17,17 @@ void env_destroy(t_env *env)
 {
     (void)env;
 }
+
+t_env *env_find(t_env *env,
+                const char *key)
+{
+    while (env)
+    {
+        if (strcmp(env->key, key) == 0)
+            return (env);
+
+        env = env->next;
+    }
+
+    return (NULL);
+}
