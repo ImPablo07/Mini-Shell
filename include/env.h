0efbbe902b9b842c1c00a829abfacc41edbf8848
Paweł Czarny <141430393+ImPablo07@users.noreply.github.com>
2026-06-11 20:48:14 +0200

    Fix duplicate declaration of env_get function

--- include/env.h
+++ include/env.h
@@ -9,17 +9,14 @@ typedef struct s_env
 } t_env;
 
 t_env  *env_init(char **envp);
-char   *env_get(t_env *env, const char *key);
 void    env_destroy(t_env *env);
 
 t_env   *env_find(t_env *env, const char *key);
-
 char    *env_get(t_env *env, const char *key);
 
 int     env_set(t_env **env,
                 const char *key,
                 const char *value);
-
 int     env_unset(t_env **env,
                   const char *key);
 
